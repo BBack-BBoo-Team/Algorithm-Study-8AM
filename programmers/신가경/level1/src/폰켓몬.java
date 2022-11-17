5ca02c41a60990d01f1e14fa677bf7be01a54a97
GG <rkrudtls@gmail.com>
2022-11-17 20:36:54 +0900

    폰켓몬, 프린터

---
+++ programmers/신가경/level1/src/폰켓몬.java
@@ -0,0 +1,33 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class 폰켓몬 {
+
+
+    public static void main(String[] args) {
+
+        int[] test = {3,3,3,2,2,2};
+        System.out.println(solution(test));
+    }
+
+    static public int solution(int[] nums) {
+
+        int answer = 0;
+
+        Set<Integer> set = new HashSet<>();
+        for(int i : nums) {
+            set.add(i);
+        }
+
+        int availableNum = nums.length / 2;
+
+        if(set.size() >= availableNum) {
+            answer = availableNum;
+        } else {
+            answer = set.size();
+        }
+
+        return answer;
+    }
+
+}
