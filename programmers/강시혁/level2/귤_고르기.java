b9b1be8ca51cab6eb9551e8072b9c16f86a85797
Si-Hyeak-KANG <zlcls456@gmail.com>
2022-12-26 18:57:50 +0900

    7회차 풀이 수정

--- programmers/강시혁/level2/귤_고르기.java
+++ programmers/강시혁/level2/귤_고르기.java
@@ -19,11 +19,11 @@ public class 귤_고르기 {
         System.out.println(result); // 1
     }
 
+
     /**
      * 데이터의 범위가 1 ~ 10,000,000 임으로 완전탐색 X,
      * Greedy 알고리즘 필요
-     * 중복되는 개수가 적은 요소를 제외하면, 서로 다른 종류의 수가 최솟값으로 구할 수 있음.
-     * 반대로하면, 중복되는 개수가 많은 크기의 귤을 고르면 됨.
+     * 중복되는 개수가 클 수록, 서로 다른 종류의 최솟값
      */
     public int solution(int k, int[] tangerine) {
 
@@ -31,26 +31,24 @@ public class 귤_고르기 {
         HashMap<Integer, Integer> group = checkCount(tangerine);
 
         // 2. 정렬을 위해 배열에 개수 정보 저장
-        int[][] arr = new int[group.size()][2];
-        int index = 0;
-        for (Integer val : group.keySet()) {
-            arr[index][0] = val;
-            arr[index][1] = group.get(val);
-            index++;
+        int[][] sortedArr = new int[group.size()][2];
+        int i = 0;
+        for (Integer size : group.keySet()) {
+            sortedArr[i][0] = size;
+            sortedArr[i][1] = group.get(size);
+            i++;
         }
 
         // 3. 개수를 기준으로 내림차순 정렬
-        sortArr(arr);
+        sortArr(sortedArr);
 
         // 4. 귤의 개수만큼 K를 감소시키면서 count
         int answer = 0;
-        int currCount =0;
-        index = 0;
+        i = 0;
         while(k>0) {
-            currCount = arr[index][1];
-            k -= currCount;
+            k -= sortedArr[i][1];
             answer++;
-            index++;
+            i++;
         }
 
         return answer;
