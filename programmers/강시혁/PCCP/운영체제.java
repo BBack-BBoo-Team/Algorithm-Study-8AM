af12614d8b6529b58d748cf77a80cff860ce529b
Si-Hyeak-KANG <zlcls456@gmail.com>
2023-01-11 23:34:21 +0900

    james 9회차 과제 풀이

--- programmers/강시혁/PCCP/운영체제.java
+++ programmers/강시혁/PCCP/운영체제.java
@@ -16,44 +16,48 @@ public class 운영체제 {
         System.out.println(Arrays.toString(result));
     }
 
-    // 현재 시가 이하로 시작하는 프로그램 중, 우선순위가 낮은 것부터 실행
+    // 현재시간 이하로 시작하는 프로그램 중, 우선순위가 낮은 것부터 실행
     public long[] solution(int[][] program) {
 
         int len = program.length;
+
+        // 시작시간 기준 오름차순으로 정렬
         Arrays.sort(program, (o1, o2) -> o1[1] - o2[1]);
 
-        PriorityQueue<int[]> waiting = new PriorityQueue<>((o1, o2) ->
-                o1[0] == o2[0] ? o1[1] - o2[1] : o1[0] - o2[0]);
-
+        // 프로그램 점수 1~10 점 사이
         long[] answer = new long[11];
         int idx = 0, count = 0, time = 0, sum = 0;
 
+        // 대기 중인 프로그램 중 우선순위가 낮은 것부터 실행되기 때문에, 아래와 같이 정렬
+        PriorityQueue<int[]> waiting = new PriorityQueue<>((o1, o2) ->
+                o1[0] == o2[0] ? o1[1] - o2[1] : o1[0] - o2[0]);
+
+        // 모든 프로그램이 실행되었다는 것을 알기 위해 count 체크
         while (count < len) {
+
+            // point) 시작시간이 현재 time 이하일 경우 [대기중]으로 옮김
             for (int i = idx; i < len; i++) {
                 int[] curr = program[i];
                 int st = curr[1];
                 if (st > time) break;
                 waiting.add(curr);
-                idx++;
+                idx++; // 중복 방지
             }
-
             if (!waiting.isEmpty() && sum <= time) {
+                // [대기중] 에서 우선순위가 가장 빠른 프로그램을 poll
                 int[] curr = waiting.poll();
                 int pri = curr[0];
                 int st = curr[1];
                 int rt = curr[2];
 
+                // 현재시간에서 시작시간을 빼서 대기시간을 구한 후 ans 에 저장
                 answer[pri] += time - st;
                 sum = time + rt;
                 count++;
             }
-
             time++;
         }
         answer[0] = sum;
         return answer;
     }
-
 }
-
-
